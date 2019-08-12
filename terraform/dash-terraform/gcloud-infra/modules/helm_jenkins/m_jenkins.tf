ab4c83cb73a55eb04d5327c4518b480bc64f4ae2
knightz007 <anupkumar.menon@yahoo.com>
2019-08-12 01:05:21 -0700

    Adding rbac for jenkins namespace

--- terraform/dash-terraform/gcloud-infra/modules/helm_jenkins/m_jenkins.tf
+++ terraform/dash-terraform/gcloud-infra/modules/helm_jenkins/m_jenkins.tf
@@ -78,3 +78,43 @@ output "jenkins_deployment_name" {
   value = "${helm_release.jenkins-app-resource.name}"
 }
 
+
+resource "kubernetes_cluster_role_binding" "jenkins" {
+  metadata {
+    name = "jenkins"
+  }
+
+  subject {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "User"
+    name      = "admin"
+  }
+
+  subject {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "User"
+    name      = "kubelet"
+  }
+
+  subject {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Group"
+    name      = "system:serviceaccounts:jenkins"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  depends_on = ["kubernetes_namespace.jenkins_namespace_resource"]
+}
+
+
+
+
+
+
+
+
